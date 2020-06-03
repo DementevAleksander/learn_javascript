1be15272c430434ad4a276843dfd31d4cbc3d6a9
DementevAlexandr <dementev.aleksander@gmail.com>
2020-06-03 17:17:40 +0300

    Обновлено.

--- 02_create_attachment_2/js/original.js
+++ 02_create_attachment_2/js/original.js
@@ -15,3 +15,41 @@
 'use strict';
 
 // Код возьмите из предыдущего домашнего задания
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+          console.log(i);
+    // != null - не нажата кнопка "Отмена", != '' - не пустое значение, a.length < 50 - введено знаков не более 50.
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b; // [a] - свойство объекта, = b - значение свойства.
+        console.log('done');
+        console.log(i);      
+    } else {
+        console.log('error');
+        i--;
+        console.log(i);
+    }
+} 
+console.log(personalMovieDB);
+
+if (personalMovieDB.count <= 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+console.log(personalMovieDB);
