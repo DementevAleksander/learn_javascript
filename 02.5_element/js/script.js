16dd11dfb5a55a3cde1869cfbf737f413f76b8e5
DementevAlexandr <dementev.aleksander@gmail.com>
2020-06-13 22:28:12 +0300

    Elements.

--- 02.5_element/js/script.js
+++ 02.5_element/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 //---------------------------------------------------------------------------------------------------//
 // Устаревшие методы, но ещё встречаются в проектах.
 //---------------------------------------------------------------------------------------------------//
@@ -42,3 +43,46 @@ hearts.forEach(item => {
 //один селектор, первый элемент.
 const oneHeart = document.querySelector('.heart');
 console.log(oneHeart);
+*/
+
+
+const box777 = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circle777 = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper');
+
+console.dir(box777);
+
+// box777.style.backgroundColor = 'yellow';
+// box777.style.width = '500px';
+box777.style.cssText = 'background-color: yellow; width: 1000px;';
+
+
+btns[1].style.borderRadius = '100%';
+circle777[0].style.backgroundColor = 'yellow'; //только для конкретных элементов.
+
+/*
+for (let i = 0; i < hearts.length; i++) {
+// i = 0 - начинаем перебирать элементы по порядку, начиная с 0 индекса.
+// i < hearts.length цикл работает до тех пор, пока не останется элементы внутри этого псевдомассива.
+    hearts[i].style.backgroundColor = 'yellow';
+//  hearts[i] в цикле плучаем элементы.   
+}
+*/
+
+// Циклы используются крайне редко, так есть специальные перебирающие методы.
+hearts.forEach(item => {
+    item.style.backgroundColor = 'yellow';
+});
+
+const div = document.createElement('div'); //создание элемента на странице.
+//const text = document.createTextNode('Это я написал с JS!'); //текстовый узел,без оболочки тега.Использ. редко.
+
+//Модификация класса CSS
+div.classList.add('black'); //Добавляем класс 'black'.
+//Добавляем div в конец body.
+document.body.append(div);
+
+wrapper.append(div); //находим класс .wrapper и встаавляем div вконец.
