856b1469c2cdf19e06bb6e29c0d292d39acc1420
DementevAlexandr <dementev.aleksander@gmail.com>
2020-06-16 21:37:36 +0300

    Навигация DOM.

---
+++ 02.8_DOM_navigation/js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes); //получить все узлы DOM дерева.
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
