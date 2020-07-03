08768c6564eb90d2f2d5b4a192dd2b8f39f4a1c2
DementevAlexandr <dementev.aleksander@gmail.com>
2020-07-03 22:51:46 +0300

    Таймер на сайт.

--- 03.04_Timer/js/script.js
+++ 03.04_Timer/js/script.js
@@ -1,3 +1,4 @@
+/*
 window.addEventListener('DOMContentLoaded', function() {
 
     // Tabs
@@ -95,3 +96,71 @@ window.addEventListener('DOMContentLoaded', function() {
     setClock('.timer', deadline);
 
 });
+*/
+
+'use strict';
+window.addEventListener('DOMContentLoaded', () => { //Загружаем DOM структуру.
+    const tabs = document.querySelectorAll('.tabheader__item'), //Получаем все вкладки.
+          tabsContent = document.querySelectorAll('.tabcontent'), //Получаем весь контентент для вкладок.
+          tabsParent = document.querySelector('.tabheader'); //Получаем родитеьский элемент вкладок.
+    
+    function hideTypeContent() { //Создаём функуию, которая скрывает контент вкладки.
+        tabsContent.forEach(item => { // перебираем псевдомасив с .tabcontent
+            //item.style.display = 'none'; //скрыть каждый.tabcontent. в реальных проектах не всегда испольуются inline.
+            item.classList.add('hide'); //добавляем класс hide, который скрывает контент.
+            item.classList.remove('show', 'fade'); //добавляем класс show, который показывает контент,убираем анимацию.
+            // toggle использовать нельзя.
+        });
+        
+        tabs.forEach(item => { //Убираем класс активности. Перебираем псевдомасив с .tabheader__item
+            item.classList.remove('tabheader__item_active'); //удаляем ...._active найденный в .tabheader__item.
+        });
+    }
+
+    function showTypeContent(i = 0) { //Создаём функуию, которая показывает контент вкладки.
+        // По умолчанию выводится первый элемент.
+        // tabsContent[i].style.display = 'block'; //в реальных проектах не всегда испольуются inline стили.
+        tabsContent[i].classList.add('show', 'fade'); //добавляем класс hide, который скрывает контент + анимация.
+        tabsContent[i].classList.remove('hide'); //добавляем класс show, который показывает контент.
+        tabs[i].classList.add('tabheader__item_active'); //добавляем ...._active найденный в .tabheader__item.
+    }
+
+    hideTypeContent();
+    showTypeContent(); //по умолчанию показываем контент первой вкладки.
+
+    // Делегирование событий и назначаем обработчик событий клика.
+    // Когда кликнули в пункт меню определяем его № в списке всех табов и по этому № вызываем функцию showTypeContent(),
+    // то есть показываем контент и делается это перебором. Перебераем все табы, и сравниваем,
+    // если элемент, который находится в псевдомассиве .tabheader__item совпадает с тем элементом,
+    // который кликнул пользователь, тогда мы берём его номер и показываем на странице.
+    tabsParent.addEventListener('click', (event) => { //Нажимаем на .tabheader
+        const target = event.target;
+        if (target && target.classList.contains('tabheader__item')) { //кликнули точно на вкладку.
+            tabs.forEach((item, i) => { //перебираем массив с вкладками .tabheader__item.
+                if (target == item) { // если .tabheader__item совпадаем с элементом в .forEach, то есть item.
+                    hideTypeContent(); //то скрываем весь контент.
+                    showTypeContent(i); //показываем тот порядковый элемент i в который кликнули.
+                }
+            });
+        }
+    });
+
+    // Timer
+    const deadline = '2020-07-31'; //Это будет отправная точка. Будущая дата.
+    // Делаем функцию, которая определяет разницу между деадлайном и текущим временем.
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()); //получаем количество милисекунд до конечной даты от ткущей даты.
+        //Превращаем количество милисекунд в формат Дни/Часы/Минуты/Секунды.
+        const days = Math.floor(t / (1000 * 60 * 60 * 24)); //Подсчитать количество дней, которые будут отображаться в таймере. 
+        // Math.floor() - округление до целого числа.
+        const hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+              
+    }
+
+
+
+});
+
+
