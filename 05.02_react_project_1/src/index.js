d40c8219e6f464231d14af1755ce49418bf70998
DementevAlexandr <dementev.aleksander@gmail.com>
2020-09-01 19:46:10 +0300

    Reactjs.

--- 05.02_react_project_1/src/index.js
+++ 05.02_react_project_1/src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 
-const Elem = () => {
-  return <h2>Привет, мир!</h2>
-}
+// const Elem = () => {
+//   return <h2>Привет, мир!</h2>
+// }
 
-const App = () => {
-  return (
-    <div>
-      <Elem/>
-    </div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div>
+//       <Elem/>
+//     </div>
+//   )
+// }
 
 ReactDOM.render(
 
