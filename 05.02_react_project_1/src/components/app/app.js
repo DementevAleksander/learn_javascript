82ea2205326946bb55f0f3ce4cbb86b827246251
DementevAlexandr <dementev.aleksander@gmail.com>
2020-09-05 20:17:29 +0300

    ReactJS.

--- 05.02_react_project_1/src/components/app/app.js
+++ 05.02_react_project_1/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -8,25 +8,63 @@ import PostAddForm from '../post-add-form';
 
 import './app.css';
 
-const App = () => { //Компонент с приложением
+export default class App extends Component { //Компонент с приложением
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {label: "Going to learn React", important: true, id: 1},
+                {label: "That is so good", important: false, id: 2},
+                {label: "I need a break...", important: false, id: 3},
+                {label: "4", important: false, id: 4}
+            ]
+        };
+        this.deletedItem = this.deletedItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+        this.maxId = 5;
+    }
 
-    const data = [
-        {label: "Going to learn React", important: true, id: 1},
-        {label: "That is so good", important: false, id: 2},
-        {label: "I need a break...", important: false, id: 3}
-    ];
+    deletedItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+            const newArr = [...before, ...after];
+            return {
+                data: newArr
+            }
+        });
+    }
 
-    return (
-        <div className="app">
-            <AppHeader />
-            <div className="search-panel d-flex">
-                <SearchPanel />
-                <PostStatusFilter />
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader />
+                <div className="search-panel d-flex">
+                    <SearchPanel />
+                    <PostStatusFilter />
+                </div>
+                <PostList 
+                    posts={this.state.data}
+                    onDeleted={this.deletedItem} />
+                <PostAddForm
+                    onAdd={this.addItem} />
             </div>
-            <PostList posts={data} />
-            <PostAddForm />
-        </div>
-    )
+        )
+    }
 }
-
-export default App;
--- 05.02_react_project_1/src/components/post-list/post-list.js
+++ 05.02_react_project_1/src/components/post-list/post-list.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDeleted}) => {
 
     const elements = posts.map((item) => {
         return (
             <li key={item.id} className='list-group-item'>
                 <PostListItem
-                label={item.label}
-                important={item.important} />
+                    label={item.label}
+                    important={item.important}
+                    onDeleted={() => onDeleted(item.id)} />
             </li>
         )
     });
