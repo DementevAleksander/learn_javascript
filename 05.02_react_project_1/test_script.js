ccca4c180fe08167fd1845c7945deddf579d9a98
DementevAlexandr <dementev.aleksander@gmail.com>
2020-10-24 21:30:41 +0300

    Мини проект react.

---
+++ 05.02_react_project_1/test_script.js
@@ -0,0 +1,9 @@
+const data = [
+    {label: "Прошлое забыто, будущее закрыто, настоящее даровано", important: true, id: 1},
+    {label: "Там, где есть руины, есть надежда на сокровища", important: false, id: 2},
+    {label: "У тихих людей самые громкие мысли", important: false, id: 3},
+    {label: "Примите неопределенность. Когда ничего не ясно, все возможно", important: false, id: 4}
+];
+
+const data1 = {data};
+console.log(data1);
