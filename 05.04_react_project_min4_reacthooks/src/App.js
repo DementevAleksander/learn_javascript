ba3b4a6be36ec0fec6553d5c432207e3bb851cb8
DementevAlexandr <dementev.aleksander@gmail.com>
2021-01-24 18:25:58 +0300

    ReactJS App. Learn. React Hooks. useEffect.

--- 05.04_react_project_min4_reacthooks/src/App.js
+++ 05.04_react_project_min4_reacthooks/src/App.js
@@ -1,28 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 function App() {
 
-  const [counter, setCounter] = useState(100) // 1 - сам state, 2 - функция, которая отслеживает, что state изменился.
-  console.log(counter, setCounter)
-
-  function increment() {
-    // setCounter(counter + 1) //вызываем функцию изменения state
-    setCounter((prevCounter) => { //получаем предыдущее состояние state.
-      return prevCounter + 1
-    })
-    setCounter(prevCounter => prevCounter + 1) //тоже самое, что и в предыдущем вызове setCounter()
-
-  }
-
-  function decrement() {
-    setCounter(counter - 1) //вызываем функцию изменения state
-  }
-
   return (
     <div>
-      <h1>Счётчик: {counter}</h1>
-      <button onClick={increment} className="btn btn-success">Добавить</button>
-      <button onClick={decrement} className="btn btn-danger">Убрать</button>
+      
     </div>
   );
 }
