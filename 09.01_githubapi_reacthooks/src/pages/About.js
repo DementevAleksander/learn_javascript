4f3bcf70353f082c89d872a96f1fbf358e925b84
DementevAlexandr <dementev.aleksander@gmail.com>
2021-01-26 22:10:44 +0300

    Project ReactJS. GitHub API. React Hooks. Навигация, роутинг, главная страница, предупреждение, поиск.

---
+++ 09.01_githubapi_reacthooks/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+
+  return (
+<div className="jumbotron">
+  <div className="container">
+    <h1 className="display-4">Информация о приложении</h1>
+    <p className="lead">Приложение версии <strong>1.0.0.</strong></p>
+  </div>
+</div>
+  )
+}
